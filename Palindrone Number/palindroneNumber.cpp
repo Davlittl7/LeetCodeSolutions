b6c26adf6f14403379ca9c70adc608168b285816
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2023-07-31 19:45:25 -0400

    Update palindroneNumber.cpp

--- Palindrone Number/palindroneNumber.cpp
+++ Palindrone Number/palindroneNumber.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        //Convert int number into a string to traverse it 
         string a = to_string(x);
+
+        //Variables to represent the start and end of the string 
         int start = 0; int end = a.length() - 1;
+
+        //While loop that continues until the start reaches the end of the string
         while(start != a.length() - 1) {
-            if(a[start] != a[end]) return false;
-            ++start; --end;
+            //Checks to see if the current start character is equal to current end character
+            //Returns false if not 
+            if(a[start] != a[end]) return false; 
+            ++start; --end; //Moves start and end to continue comparisons 
         }
+        //Returns true if code makes it out of the while loop
         return true;
     }
 };
