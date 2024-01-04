04d57e99e743c4cc31a69f515bdf712b3cc66f58
Davlittl7 <70984454+Davlittl7@users.noreply.github.com>
2024-01-03 19:02:13 -0500

    Update removeElement.cpp

--- Remove Element/removeElement.cpp
+++ Remove Element/removeElement.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int countNewTotal = 0;
-        int replacementNum = val + 1;
-        int i = 0;
-        int numSize = nums.size();
+        int countNewTotal = 0; //Hold total nums that are not equal to val
+        int replacementNum = val + 1; //What a number will be replaced with if it is equal to val
+        int i = 0; //Indexing purposes in while loop
+        int numSize = nums.size(); //Holds size of vector in modifiable format
         while(i < numSize) {
-            if(nums[i] != val) {
+            if(nums[i] != val) { //Increments if current num isn't the val
                 ++countNewTotal;
                 ++i;
             }
-            else {
+            //Erases val element and pushes back a new replacement num and shrinks numSize 
+            //so that they are not counted to countNewTotal var
+            else { 
                 nums.push_back(replacementNum);
                 nums.erase(nums.begin() + i);
                 --numSize;
